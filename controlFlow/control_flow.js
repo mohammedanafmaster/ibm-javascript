ce445a1c3ed4027ddc4333b4f2e71f9ff712bdbc
mohammedanafmaster <mohammedanafm@gmail.com>
2024-07-03 04:34:04 -0400

    Practice task source code added

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -59,4 +59,55 @@ console.log("Authentication Status: ", authenticationStatus);
 
 // Practice task
 
+// if...else condition
+let employment = "employee";
+let authorizationStatus;
 
+if (employment === "employee") {
+    authorizationStatus = "You have access to \"Dietary Services\".";
+} else {
+    authorizationStatus = "You do not have access to \"Dietary Services\".";
+}
+
+console.log("Authorization Status: ", authorizationStatus);
+
+// Nested if...else condition
+let subscription = "enrolled member";
+let subscriptionStatus;
+
+if (employment === "employee") {
+    if (subscription === "enrolled member") {
+        subscriptionStatus = "You have access to \"Dietary Services\" and can have one-on-one interaction with a dietician.";
+    } else {
+        subscriptionStatus = "You have access to \"Dietary Services\" but can not have one-on-one interaction with a dietician.";
+    }
+} else {
+    subscriptionStatus = "You can not have access to \"Dietary Services\".";
+}
+
+console.log("Subscription Status: ", subscriptionStatus);
+
+// Switch statement
+let membership = "subscriber";
+let membershipStatus;
+
+switch (membership) {
+    case "admin":
+        membershipStatus = "You have full access to facilitate \"Dietary Services\".";
+        break;
+
+    case "subscriber":
+        membershipStatus = "You have partial access to facilitate \"Dietary Services\".";
+        break;
+
+    default:
+        membershipStatus = "You do not have access to facilitate \"Dietary Services\".";
+}
+
+console.log("Membership Status: ", membershipStatus);
+
+// Ternary operator
+let enrollment = "non-subscriber";
+let enrollmentStatus = enrollment ? "You need to enroll or at least subscribe first to avail this facility." : "You have already enrolled and can avail this facility.";
+
+console.log("Enrollment Status: ", enrollmentStatus);
