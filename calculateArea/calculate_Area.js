90c842bb2d0bc686d3e69a7b4251286bf06bd6c2
mohammedanafmaster <mohammedanafm@gmail.com>
2024-07-03 06:20:25 -0400

    Practice task code added

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -8,3 +8,16 @@ function calculateArea() {
 
     document.getElementById("result").innerText = `The area of rectangle is ${area}`;
 }
+
+// Practice task
+let grocery1, grocery2, grocery3, total;
+
+const groceryTracker = () => {
+    grocery1 = parseFloat(document.getElementById("grocery1").value);
+    grocery2 = parseFloat(document.getElementById("grocery2").value);
+    grocery3 = parseFloat(document.getElementById("grocery3").value);
+
+    total = grocery1 + grocery2 + grocery3;
+
+    document.getElementById("output").innerText = `The total amount is: \$${total}`
+};
